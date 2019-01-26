d88bc60d968559a3518f8151d55f499a57e7fc74
William Tumeo <tumeowilliam@gmail.com>
2019-01-26 01:51:06 -0200

    Update tests

--- gdscript/test-errors.sh
+++ gdscript/test-errors.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/sh
 
 echo "Assertion tests ->"
-python gdscript.py oneline "assert(false)"
-python gdscript.py block "
+python gdscript.py -o "assert(false)"
+python gdscript.py "
 extends Node
 
 func _ready():
     assert(false)
 "
-python gdscript.py file 'test/errors/Assertion.gd'
+python gdscript.py 'test/errors/Assertion.gd'
 
 echo ""
 echo "Identifier Not Found tests ->"
-python gdscript.py oneline "print(i)"
-python gdscript.py block "
+python gdscript.py -o "print(i)"
+python gdscript.py "
 extends Node
 
 func _ready():
     print(i)
 "
-python gdscript.py file 'test/errors/Identifier.gd'
+python gdscript.py 'test/errors/Identifier.gd'
 
 echo ""
 echo "Type tests ->"
-python gdscript.py oneline "print('1'+1)"
-python gdscript.py block "
+python gdscript.py -o "print('1'+1)"
+python gdscript.py "
 extends Label
 
 func _ready():
@@ -33,8 +33,8 @@ func _ready():
 
 echo ""
 echo "Memory leak tests ->"
-python gdscript.py oneline "Node.new()"
-python gdscript.py block "
+python gdscript.py -o "Node.new()"
+python gdscript.py "
 extends Node
 
 func _ready():
--- gdscript/test-scripts.sh
+++ gdscript/test-scripts.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 
 echo "One line tests ->"
-python gdscript.py oneline "print('Hello World from command-line!')"
-python gdscript.py oneline "print(Engine.get_version_info())"
-python gdscript.py oneline "print(OS.get_screen_size(0))"
+python gdscript.py -o "print('Hello World from command-line!')"
+python gdscript.py -o "print(Engine.get_version_info())"
+python gdscript.py -o "print(OS.get_screen_size(0))"
 
 echo ''
 echo "Block tests ->"
-python gdscript.py block "
+python gdscript.py "
 extends Node
 
 func _ready():
@@ -19,8 +19,8 @@ func _ready():
 
 echo ''
 echo "Script file tests ->"
-python gdscript.py file 'test/Bhaskara.gd'
-python gdscript.py file 'test/EvaluateData.gd'
-python gdscript.py file 'test/Import.gd'
-python gdscript.py file 'test/Extends.gd'
-python gdscript.py file 'test/Enums.gd'
+python gdscript.py 'test/Bhaskara.gd'
+python gdscript.py 'test/EvaluateData.gd'
+python gdscript.py 'test/Import.gd'
+python gdscript.py 'test/Extends.gd'
+python gdscript.py 'test/Enums.gd'
